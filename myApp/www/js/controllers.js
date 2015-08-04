e8c2c606ecba3ada796cd96c434da1f33b69c08f
AlexFine <alexkfine2@gmail.com>
2015-08-04 15:09:07 -0700

    Added popup confirmation and different places

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -1,28 +1,187 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function ($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+.controller('ChatsCtrl', function ($scope, Chats) {
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+    $scope.chats = Chats.all();
+    $scope.remove = function (chat) {
+        Chats.remove(chat);
+    };
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+.controller('ChatDetailCtrl', function ($scope, $stateParams, Chats,  $ionicPopup, $timeout) {
+    $scope.chat = Chats.get($stateParams.chatId);
+    // An alert dialog
+
+    /*
+      $scope.zero = [
+    {'ride' : 'Nexus S'},
+    {'ride' : 'Motorola XOOM with WiFi'},
+    {'ride' : 'Motoral XOOMIZOOM'}
+  ];
+        $scope.one = [
+    {'ride' : 'Nexus S'},
+    {'ride' : 'Motorola XOOM with WiFi'},
+    {'ride' : 'Motoral XOOMIZOOM'}
+  ];
+        $scope.two = [
+    {'ride' : 'Nexus S'},
+    {'ride' : 'Motorola XOOM with WiFi'},
+    {'ride' : 'Motoral XOOMIZOOM'}
+  ];
+        $scope.three = [
+    {'ride' : 'Nexus S'},
+    {'ride' : 'Motorola XOOM with WiFi'},
+    {'ride' : 'Motoral XOOMIZOOM'}
+  ];
+            $scope.four = [
+    {'ride' : 'Nexus S'},
+    {'ride' : 'Motorola XOOM with WiFi'},
+    {'ride' : 'Motoral XOOMIZOOM'}
+  ];
+  */
+ 
+    $scope.showPopup = function () {
+        $scope.data = {}
+
+        // An elaborate, custom popup
+        var myPopup = $ionicPopup.show({
+            template: '<center><h5>Welcome to {{chat.ride1}}!</h5><p>You may now skip all the lines and show this app for confirmation.</p><img src="http://beam.la/qrcode.png"></center>',
+//            title: 'Welcome to {{chat.ride1}}!',
+//            subTitle: 'You may now skip all the lines and show this app for confirmation.',
+            cssClass: 'popup',
+            scope: $scope,
+            /*buttons: [
+                {
+                    text: 'Cancel'
+                },
+                {
+                    text: '<b>Confirm?</b>',
+                    type: 'button-positive',
+                    onTap: function (e) {
+                        if (!$scope.data.wifi) {
+                            //don't allow the user to close unless he enters wifi password
+                            e.preventDefault();
+                        } else {
+                            return $scope.data.wifi;
+                        }
+                    }
+       },
+     ]*/
+        });
+        myPopup.then(function (res) {
+            console.log('Tapped!', res);
+        });
+        $timeout(function () {
+            myPopup.close(); //close the popup after 3 seconds for some reason
+        }, 5000);
+    };
+    $scope.showPopup2 = function () {
+        $scope.data = {}
+
+        // An elaborate, custom popup
+        var myPopup = $ionicPopup.show({
+            template: '<center><h5>Welcome to {{chat.ride2}}!</h5><p>You may now skip all the lines and show this app for confirmation.</p><img src="http://beam.la/qrcode.png"></center>',
+//            title: 'Welcome to {{chat.ride2}}!',
+//            subTitle: 'You may now skip all the lines and show this app for confirmation.',
+            cssClass: 'popup',
+            scope: $scope,
+            /*buttons: [
+                {
+                    text: 'Cancel'
+                },
+                {
+                    text: '<b>Confirm?</b>',
+                    type: 'button-positive',
+                    onTap: function (e) {
+                        if (!$scope.data.wifi) {
+                            //don't allow the user to close unless he enters wifi password
+                            e.preventDefault();
+                        } else {
+                            return $scope.data.wifi;
+                        }
+                    }
+       },
+     ]*/
+        });
+        myPopup.then(function (res) {
+            console.log('Tapped!', res);
+        });
+        $timeout(function () {
+            myPopup.close(); //close the popup after 3 seconds for some reason
+        }, 5000);
+    };
+    $scope.showPopup3 = function () {
+        $scope.data = {}
+
+        // An elaborate, custom popup
+        var myPopup = $ionicPopup.show({
+            template: '<center><h5>Welcome to {{chat.ride3}}!</h5><p>You may now skip all the lines and show this app for confirmation.</p><img src="http://beam.la/qrcode.png"></center>',
+//            title: 'Welcome to {{chat.ride3}}!',
+//            subTitle: 'You may now skip all the lines and show this app for confirmation.',
+            cssClass: 'popup',
+            scope: $scope,
+            /*buttons: [
+                { 
+                    text: 'Cancel'
+                },
+                {
+                    text: '<b>Confirm?</b>',
+                    type: 'button-positive',
+                    onTap: function (e) {
+                        if (!$scope.data.wifi) {
+                            //don't allow the user to close unless he enters wifi password
+                            e.preventDefault();
+                        } else {
+                            return $scope.data.wifi;
+                        }
+                    }
+       },
+     ]*/
+        });
+        myPopup.then(function (res) {
+            console.log('Tapped!', res);
+        });
+        $timeout(function () {
+            myPopup.close(); //close the popup after 3 seconds for some reason
+        }, 5000);
+    };
+    // A confirm dialog
+    $scope.showConfirm = function () {
+        var confirmPopup = $ionicPopup.confirm({
+            title: 'Consume Ice Cream',
+            template: 'Are you sure you want to eat this ice cream?'
+        });
+        confirmPopup.then(function (res) {
+            if (res) {
+                console.log('You are sure');
+            } else {
+                console.log('You are not sure');
+            }
+        });
+    };
+
+    $scope.showAlert = function () {
+        var alertPopup = $ionicPopup.alert({
+            title: 'You may skip this line!',
+            template: 'This user successfully skipped the line'
+        });
+        alertPopup.then(function (res) {
+            console.log('Thank you for not eating my delicious ice cream cone');
+        });
+    };
 })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+.controller('AccountCtrl', function ($scope) {
+    $scope.settings = {
+        enableFriends: true
+    };
 });
--- myApp/www/js/services.js
+++ myApp/www/js/services.js
@@ -1,51 +1,66 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
+.factory('Chats', function () {
+    // Might use a resource here that returns a JSON array
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Disney World',
-    lastText: 'Go to the enchanted world',
-    face: 'http://beam.la/disneyworld.png',
-    rides: ['one', 'two', 'three']
+    // Some fake testing data
+    var chats = [{
+        id: 0,
+        name: 'Disney World',
+        lastText: 'Go to the enchanted world',
+        face: 'http://beam.la/disneyworld.png',
+        //rides: ['one', 'two', 'three'],
+        ride1: 'Magic Kingdom',
+        ride2: 'Astro Orbiter',
+        ride3: 'Cinderella Castle'
   }, {
-    id: 1,
-    name: 'Lego Land',
-    lastText: 'Welcome to lego land! The land of legos!',
-    face: 'http://beam.la/legoland.png'
+        id: 1,
+        name: 'Lego Land',
+        lastText: 'Welcome to lego land! The land of legos!',
+        face: 'http://beam.la/legoland.png',
+        ride1: 'Sky Cruiser',
+        ride2: 'Sky Patrol',
+        ride3: 'Beetle Bounce'
   }, {
-    id: 2,
-    name: 'Disney Land',
-    lastText: "It's the land of Disney!",
-    face: 'https://beam.la/disneyland.png'
+        id: 2,
+        name: 'Disney Land',
+        lastText: "It's the land of Disney!",
+        face: 'http://beam.la/disneyland.png',
+        ride1: 'Haunted Mansion',
+        ride2: 'Splash Mountain',
+        ride3: 'Space Mountain'
   }, {
-    id: 3,
-    name: 'Great America',
-    lastText: 'A part of the great America!',
-    face: 'http://beam.la/greatamerica.png'
+        id: 3,
+        name: 'Great America',
+        lastText: 'A part of the great America!',
+        face: 'http://beam.la/greatamerica.png',
+        ride1: 'Flight Deck',
+        ride2: 'Vortex',
+        ride3: 'Tiki Twirl'
   }, {
-    id: 4,
-    name: 'Six Flags',
-    lastText: 'There are six flags on our property.',
-    face: 'http://beam.la/sixflags.png'
+        id: 4,
+        name: 'Six Flags',
+        lastText: 'There are six flags on our property.',
+        face: 'http://beam.la/sixflags.png',
+        ride1: 'Batman',
+        ride2: 'Apocalypse',
+        ride3: 'Gren Lantern'
   }];
 
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+    return {
+        all: function () {
+            return chats;
+        },
+        remove: function (chat) {
+            chats.splice(chats.indexOf(chat), 1);
+        },
+        get: function (chatId) {
+            for (var i = 0; i < chats.length; i++) {
+                if (chats[i].id === parseInt(chatId)) {
+                    return chats[i];
+                }
+            }
+            return null;
         }
-      }
-      return null;
-    }
-  };
+    };
 });
