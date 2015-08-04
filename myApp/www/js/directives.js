c9a3fd029b1384c8c297410b46ab4b30f76749fa
AlexFine <alexkfine2@gmail.com>
2015-08-04 11:00:33 -0700

    blurring and unbarring stuff

--- myApp/www/js/directives.js
+++ myApp/www/js/directives.js
@@ -10,8 +10,8 @@ angular.module('starter.directives', [])
             'background-image': 'url(' + url +')',
             'height' : '120px',
             'background-size' : 'cover',
-            //'-webkit-filter' : 'blur(3px)', 
-            //'filter' : 'blur(3px)',
+//            '-webkit-filter' : 'blur(3px)', 
+//            'filter' : 'blur(3px)',
             'z-index' : '1',
             'display' : 'block'
         });
@@ -20,12 +20,8 @@ angular.module('starter.directives', [])
 
 .directive('unBlur', function(){
     return function(scope, element, attrs){
-        var url = attrs.backImg;
         var content = element.find('a');
         content.css({
-            'background-image': 'url(' + url +')',
-            'height' : '120px',
-            'background-size' : 'cover',
             '-webkit-filter' : 'blur(0px)', 
             'filter' : 'blur(0px)'
         });
--- myApp/www/js/services.js
+++ myApp/www/js/services.js
@@ -8,27 +8,28 @@ angular.module('starter.services', [])
     id: 0,
     name: 'Disney World',
     lastText: 'Go to the enchanted world',
-    face: 'http://4alltravelers.madadventuresinc.netdna-cdn.com/wp-content/uploads/2015/04/disney-world-walt-disney-world-34101377-500-334.jpg'
+    face: 'http://beam.la/disneyworld.png',
+    rides: ['one', 'two', 'three']
   }, {
     id: 1,
     name: 'Lego Land',
     lastText: 'Welcome to lego land! The land of legos!',
-    face: 'http://www.legoland.com.my/Global/LEGOLAND%20MY/2012/Explore/Activities/The%20Beginning/LEGOLANDArc2%20resize.jpg'
+    face: 'http://beam.la/legoland.png'
   }, {
     id: 2,
     name: 'Disney Land',
     lastText: "It's the land of Disney!",
-    face: 'https://secure.parksandresorts.wdpromedia.com/resize/mwImage/1/900/360/90/wdpromedia.disney.go.com/media/wdpro-assets/dlr/parks-and-tickets/destinations/disneyland-park/disneyland-00-full.jpg?15052013102112'
+    face: 'https://beam.la/disneyland.png'
   }, {
     id: 3,
     name: 'Great America',
     lastText: 'A part of the great America!',
-    face: 'http://brotherrice.org/wp-content/uploads/2012/09/Six_Flags_Great_America.jpeg'
+    face: 'http://beam.la/greatamerica.png'
   }, {
     id: 4,
     name: 'Six Flags',
     lastText: 'There are six flags on our property.',
-    face: 'http://www3.nd.edu/~class10/Class%20of%202010/Six%20Flags_files/2004042511484000_six-flags-trip-4-25-2004-078.png'
+    face: 'http://beam.la/sixflags.png'
   }];
 
   return {
